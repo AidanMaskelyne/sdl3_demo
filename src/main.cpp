a2b06addae86356241f1803f455f2d8b3a41f87f
Aidan M <aidan@muttleyville.org>
2024-03-12 09:27:36 +0000

    Fix building of SDL3 and displaying a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
 
 int main(int argc, char *argv[])
 {
 	SDL_Version sdl_version;
 	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Event event;
 
 	if (SDL_Init(SDL_INIT_VIDEO || SDL_INIT_AUDIO) != 0)
 	{
@@ -14,16 +20,31 @@ int main(int argc, char *argv[])
 	SDL_GetVersion(&sdl_version);
 	SDL_Log("Using SDL version %u.%u.%u", sdl_version.major, sdl_version.minor, sdl_version.patch);
 
-	window = SDL_CreateWindow("SDL3 Demo", 640, 480, SDL_WINDOW_FULLSCREEN||SDL_WINDOW_VULKAN);
-	if (window == NULL)
+	if (SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_FULLSCREEN, &window, &renderer) != 0)
 	{
-		SDL_Log("Unable to create a window: %s", SDL_GetError());
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateWindowAndRenderer failed: %s", SDL_GetError());
 		return 1;
 	}
 
-	SDL_Delay(3000);
+	if (window == NULL || renderer == NULL)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create a window/renderer: %s", SDL_GetError());
+		return 1;
+	}
+
+	while (true)
+	{
+		SDL_PollEvent(&event);
+		if (event.type == SDL_EVENT_QUIT)
+			break;
+
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
 
 	// --------------------------------------
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
